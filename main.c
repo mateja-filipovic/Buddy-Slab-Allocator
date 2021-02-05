35c8b86c093e01220248d52c57e7ae765d2656c3
Mateja Filipovic <fil.mateja@gmail.com>
2021-02-05 12:04:32 +0100

    more testing

---
+++ main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "slab.h"
+#include "buddy.h"
+#include "cache.h"
+
+
+int main() {
+
+	int sz = 1000; //267 PUCA SA NEPARNIM BROJEM BLOKOVA WTFFF
+	void* space = malloc(sz * BLOCK_SIZE);
+	kmem_init(space, sz);
+	
+	kmem_cache_s* ch = kmem_cache_create("PCB", 69, NULL, NULL);
+	kmem_cache_s* ch2 = kmem_cache_create("SEM", 70, NULL, NULL);
+	kmem_cache_s* ch3 = kmem_cache_create("SEM", 30, NULL, NULL);
+	void* objects[58];
+	void* objects2[20];
+	void* objects3[30];
+	int i;
+
+	for (i = 0; i < 58; i++)
+		objects[i] = kmem_cache_alloc(ch);
+	for (i = 0; i < 20; i++)
+		objects2[i] = kmem_cache_alloc(ch2);
+	for (i = 0; i < 30; i++)
+		objects3[i] = kmem_cache_alloc(ch3);
+
+	void* cache_overflow_ptr = kmem_cache_alloc(ch);
+
+	printf("CACHE 1 INFO!!\n");
+	print_cache(ch);
+	printf("cache 2\n");
+	print_cache(ch2);
+
+	for (i = 0; i < 58; i++)
+		kmem_cache_free(ch, objects[i]);
+	for (i = 0; i < 20; i++)
+		kmem_cache_free(ch2, objects2[i]);
+	for (i = 0; i < 30; i++)
+		kmem_cache_free(ch3, objects3[i]);
+
+	print_cache(ch);
+
+	printf("Freed %d blocks from cache!\n", kmem_cache_shrink(ch));
+	printf("Freed %d blocks from cache!\n", kmem_cache_shrink(ch2));
+	printf("Freed %d blocks from cache!\n", kmem_cache_shrink(ch3));
+	print_cache(ch);
+
+	kmem_cache_destroy(ch);
+	kmem_cache_destroy(ch2);
+
+	free(space);
+
+	/*
+	print_buddy();
+	void* ptr1 = buddy_allocate(1);
+	print_buddy();
+	void* ptr2 = buddy_allocate(1);
+	print_buddy();
+	void* ptr3 = buddy_allocate(1);
+	print_buddy();
+	void* ptr4 = buddy_allocate(1);
+	print_buddy();
+
+	printf("\n\nposle dealloca\n");
+	buddy_deallocate(ptr1, 1);
+	print_buddy();
+
+	void* ptr5 = buddy_allocate(1);
+	print_buddy();
+	*/
+	return 0;
+}
