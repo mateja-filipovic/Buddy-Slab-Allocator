c3f98dc75b8e0713f7a8c8145918e451af1205b0
Mateja Filipovic <fil.mateja@gmail.com>
2021-02-26 19:11:09 +0100

    reformatting

--- buddy.c
+++ buddy.c
@@ -10,12 +10,14 @@ void buddy_init(void* space, int numOfBlocks) {
 	//set everything to 0
 	memset(space, 0, numOfBlocks * BLOCK_SIZE);
 	BUDDY = (buddy_s*)space;
+
 	//initialize struct memebers
 	BUDDY->base_addr = space;
 	BUDDY->starting_addr = (char*)space + BLOCK_SIZE;
 	BUDDY->ending_addr = (char*)space + numOfBlocks * BLOCK_SIZE;
 	BUDDY->blocks_total = numOfBlocks - 1;
 	BUDDY->max_order = (int)ceil(log(BUDDY->blocks_total) / log(2));
+
 	//initialize the pool to 0
 	int i;
 	for (i = 0; i < POOL_MAX_SIZE; i++)
@@ -38,24 +40,16 @@ void buddy_init(void* space, int numOfBlocks) {
 	}
 }
 
-void print_buddy() {
-	printf("Buddy base address: %p\n", BUDDY->base_addr);
-	printf("Buddy start address: %p\n", BUDDY->starting_addr);
-	printf("Buddy end address: %p\n", BUDDY->ending_addr);
-	printf("Buddy block capacity: %d\n", BUDDY->blocks_total);
-	unsigned int i;
-	for (i = 0; i < BUDDY->max_order + 1; i++)
-		printf("%d -> %p\n", i, BUDDY->pool[i]);
-}
-
 void* buddy_allocate(int sizeInBlocks){
 	int i, order;
 	void* block, * bud;
 	i = 0;
+
 	//find the minimum size in blocks
 	while (_get_blk_size(i) < sizeInBlocks)
 		i++;
 	order = i;
+
 	//find the first big enough chunk available
 	for (;; i++) {
 		if (i > BUDDY->max_order)
@@ -63,6 +57,7 @@ void* buddy_allocate(int sizeInBlocks){
 		if (BUDDY->pool[i])
 			break;
 	}
+
 	block = BUDDY->pool[i];
 	BUDDY->pool[i] = *(void**)BUDDY->pool[i];
 	while (i-- > order) {
@@ -74,14 +69,18 @@ void* buddy_allocate(int sizeInBlocks){
 
 void buddy_deallocate(void* blkAddr, int sizeInBlocks){
 	int i;
+
 	//find the order of the block
 	for (i = 0; _get_blk_size(i) < sizeInBlocks; i++);
 	void* bud = _get_buddy_of(blkAddr, i);
+
 	//add it to the pool of free blocks
 	void** p = &BUDDY->pool[i];
+
 	//find the buddy in the same pool index
 	while ((*p != NULL) && (*p != bud))
 		p = (void**)*p;
+
 	//if buddy is not found/free
 	if (bud > BUDDY->ending_addr || *p != bud) {
 		*(void**)blkAddr = BUDDY->pool[i];
--- buddy.h
+++ buddy.h
@@ -9,13 +9,14 @@ typedef struct buddy_s {
 	void* base_addr; //first address in the space given
 	void* starting_addr; //the address of the first free block (buddy metadata takes up 1 block)
 	void* ending_addr; //used for error checking
+
 	unsigned long blocks_total;
 	unsigned int max_order;
+
 	void* pool[32];
 }buddy_s;
 
 void buddy_init(void* space, int numOfBlocks); //initalize
-void print_buddy(); //print info
 void* buddy_allocate(int sizeInBlocks); //allocate space given block size needed
 void buddy_deallocate(void* blkAddr, int sizeInBlocks); //deallocate space given startign address adn block size needed
 
