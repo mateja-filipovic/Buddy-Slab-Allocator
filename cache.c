4958f5c21718ddfbc77f5675887fc08dca566b0c
Mateja Filipovic <fil.mateja@gmail.com>
2021-02-26 14:48:54 +0100

    deallocation bugfixes, added more comms

--- cache.c
+++ cache.c
@@ -1,53 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "cache.h"
+#include <string.h>
 
 #include "slab.h"
 
-#define MIN_OBJ_SIZE 4
+#define MAX_VECT_SIZE 512 //512 * 8 = 4096 maximum
 
-
-slab_s* allocate_a_slab(int obj_size, int* ret_param) {
+slab_s* allocate_a_slab(int obj_size, int* objs_per_slab_ret, void(*ctor)(void*), int* wasted_ret, int offs) {
 	//get the number of blocks needed
-	int block_num = (sizeof(slab_s) + 10 * obj_size) / BLOCK_SIZE;
-	if (sizeof(slab_s) + 10 * obj_size % BLOCK_SIZE != 0)
+	int block_num = (10 * obj_size) / BLOCK_SIZE;
+	if (10 * obj_size % BLOCK_SIZE != 0)
 		block_num++;
 
-	slab_s* sl = buddy_allocate(block_num);
+	slab_s* sl = buddy_allocate(1);
 	//error checking
 	if (sl == NULL) {
-		printf("Slab err: Buddy has no space to allocate another slab!\n");
+		printf("Slab error: Buddy has no space to allocate another slab manager!\n");
 		return NULL;
 	}
 
-	sl->base_addr = sl;
-	sl->starting_addr = (char*)sl->base_addr + sizeof(slab_s);      //always cast to char* cos of pointer arithmetic!!
+	sl->base_addr = buddy_allocate(block_num);
+	if (sl->base_addr == NULL) {
+		printf("Slab error: Can't allocate another slab!\n");
+		return NULL;
+	}
+
+	sl->starting_addr = (char*)sl->base_addr + offs;      //always cast to char* cos of pointer arithmetic!!
 	sl->ending_addr = (char*)sl->base_addr + BLOCK_SIZE * block_num;
-	sl->first_free = sl->starting_addr;
 
-	*ret_param = (block_num * BLOCK_SIZE - sizeof(slab_s)) / obj_size;
+	if(*objs_per_slab_ret == -1)
+		*objs_per_slab_ret = (block_num * BLOCK_SIZE - sizeof(slab_s)) / obj_size;
+	if (*wasted_ret == -1) {
+		*wasted_ret = (block_num * BLOCK_SIZE) % obj_size;
+	}
 
-	if (obj_size < MIN_OBJ_SIZE)
-		obj_size = MIN_OBJ_SIZE;
 	sl->num_of_blocks = block_num;
-	sl->capacity = (block_num * BLOCK_SIZE - sizeof(slab_s)) / obj_size;
+	sl->capacity = (block_num * BLOCK_SIZE) / obj_size;
 	sl->obj_size = obj_size;
 
 	sl->num_of_allocs = 0;
 	sl->state = 0;
 
 	sl->next = NULL; //used by the cache manager!
+	sl->bit_vector = (unsigned char*)sl + sizeof(slab_s);
 	
-	//initialize the free blocks list
+	memset(sl->bit_vector, 0, MAX_VECT_SIZE); //svi su slobodni
+
+	//initialize the objects using the ctor provided!
+	sl->ctor = ctor;
+	
 	int i;
-	void** tmp = (void**)sl->first_free; //tmp stores the address of the free block
-	for (i = 0; i < sl->capacity; i++) {
-		*tmp = (char*)(tmp) + obj_size; //store the address of the next free block in the current free block
-		if (i == sl->capacity - 1) {
-			*tmp = 0; //the last free block points to 0
-			break; //to handle a nullptr warning
+	if (ctor != NULL) {
+		for (i = 0; i < sl->capacity; i++) {
+			void* objp = get_obj_address(sl, i);
+			if (objp < sl->starting_addr || objp >= sl->ending_addr)
+				printf("Slab error: Cannot construct object!");
+			else
+				(*ctor)(objp);
 		}
-		tmp = *tmp;
 	}
+
 	return sl;
 }
 
@@ -58,40 +71,34 @@ void print_slab(slab_s* sl) {
 	printf("Slab capacity: %d, allocated so far: %d, obj size: %d\n", sl->capacity, sl->num_of_allocs, sl->obj_size);
 }
 
-void* allocate_obj_from_slab(slab_s* sl, void(*ctor)(void*)){
+void* allocate_obj_from_slab(slab_s* sl) {
 	if (sl->capacity == sl->num_of_allocs)
 		return NULL;
 
+	int i = 0;
+	while (get_bit(sl, i) != 0)
+		i++;
 
-	void* ret = sl->first_free;
-	//change
-	if (sl->first_free != 0)
-		sl->first_free = *(void**)sl->first_free;
-	///chagne end, maybe needs a revert!
-
-	//sl->first_free = *(void**)sl->first_free;
-	//sl->num_of_allocs++;
+	void* ret = get_obj_address(sl, i);
+	set_bit(sl, i, 1);
 
 	sl->num_of_allocs++;
 	//change the state if necessary
 	if (sl->state == 0)
 		sl->state = 1;
-	else if (sl->num_of_allocs == sl->capacity || sl->first_free == 0) //second condition added, more error checking!
+	else if (sl->num_of_allocs == sl->capacity) //|| sl->first_free == 0) //second condition added, more error checking!
 		sl->state = 2;
 
-	//initalize the object if the constructor is provided
-	if (ctor != NULL)
-		(*ctor)(ret);
+	//just in case a dealloced object gets allocated again
+	//if (sl->ctor != NULL)
+		//(*sl->ctor)(ret);
 
 	return ret;
 }
 
 void deallocate_a_slab(slab_s* sl){
 	buddy_deallocate(sl->base_addr, sl->num_of_blocks);
-	//REVERT IF NEEDED
-	//unsigned int sz = (unsigned int)sl->ending_addr - (unsigned int)sl->base_addr;
-	//buddy_deallocate(sl->base_addr, sz);
-	//REVERT IF NEEDED
+	buddy_deallocate((void*)sl, 1);
 
 }
 
@@ -99,12 +106,14 @@ int dealloc_obj_from_slab(slab_s* sl, void* obj, void(*dtor)(void*)){
 	
 	if (sl->num_of_allocs == 0)
 		return -1; //no objs allocated
-	if (!is_obj_valid(sl, obj))
+	int obj_index = get_obj_index(sl, obj);
+	if (obj_index == -1)
 		return -2; //the obj is not from this slab
 
+
+
 	//update the free slots list
-	*(void**)obj = sl->first_free;
-	sl->first_free = obj;
+	set_bit(sl, obj_index, 0);
 
 	sl->num_of_allocs--;
 	//change the state if necessary
@@ -113,20 +122,69 @@ int dealloc_obj_from_slab(slab_s* sl, void* obj, void(*dtor)(void*)){
 	else if (sl->num_of_allocs == 0)
 		sl->state = 0;
 
-	if (dtor != NULL)
-		(*dtor)(obj);
+	/*
+	* NOTE: PROJECT REQS UNCLEAR, NOT SURE IF AN OBJECT SHOULD BE INITIALIZED
+	* AFTER BEING DEALLOCATED, OR JUST DESTRUCTED
+	* UNCOMMENT IF NECESSARY
+	* if (sl->ctor != NULL)
+		* (*sl->ctor)(obj);
+	*/
 
 	return 0;
 }
 
-int is_obj_valid(slab_s* sl, void* obj) {
-	void* tmp = sl->starting_addr;
-	while (tmp < sl->ending_addr) {
-		if (obj == tmp)
-			return 1; //object is from this slab
-		//check if an object is already free?
-		tmp = (char*)tmp + sl->obj_size;
+//get the bit vector index of an object slot
+int get_obj_index(slab_s* sl, void* obj) {
+	int index = -1;
+	if (obj >= sl->ending_addr || obj < sl->starting_addr)
+		return index;
+	int calc = (unsigned int)(obj)-(unsigned int)(sl->starting_addr);
+	if (calc % sl->obj_size != 0)
+		return index;
+	return calc / sl->obj_size;
+}
+
+//get object address using a bit vector index
+void* get_obj_address(slab_s* sl, int index){
+	void* ret = (char*)sl->starting_addr + index * sl->obj_size;
+	return ret;
+}
+
+void print_bit_vect(slab_s* sl){
+	char* bv = sl->bit_vector;
+	int i;
+	for (i = 0; i < sl->capacity; i++) {
+		printf("%d", get_bit(sl, i));
+		if ((i+1) % 8 == 0 && i != 0)
+			printf(", ");
 	}
-	return 0;
+	printf("\n");
+}
+
+void set_bit(slab_s* sl, int index, int bit) {
+	//find the word
+	int word = index >> 3;
+	int n = sizeof(index) * 8 - 3;
+	int offset = ((unsigned)index << n) >> n;
+	if (bit) {
+		sl->bit_vector[word] |= 1 << (7 - offset);
+	}
+	else {
+		sl->bit_vector[word] &= ~(1 << (7 - offset));
+	}
+}
+
+int get_bit(slab_s* sl, int index) {
+	//find the word
+	int word = index >> 3;
+	int n = sizeof(index) * 8 - 3;
+	int offset = ((unsigned)index << n) >> n;
+	//check what value the bit has
+	if (sl->bit_vector[word] & (1 << (7 - offset))) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
 
--- cache.h
+++ cache.h
@@ -3,49 +3,73 @@
 
 #include "buddy.h"
 
-#define SMALL_MEM_BUFFER_NUM
-
 typedef struct slab_s {
-	void* base_addr;
-	void* starting_addr;
+	void* base_addr; //address of the object block
+	void* starting_addr; //address of the first object in the object block
 	void* ending_addr;
-	void* first_free;
-	//unsigned int first_free;
-	int num_of_blocks;
-	int capacity;
+
+	int num_of_blocks; //how many blocks does object block take up
+	int capacity; //max num of objects
+
 	int state; // 0 - empty, 1 - partial, 2 - full
 	int num_of_allocs; //objects allocated so far
+
 	int obj_size;
+
+	unsigned char* bit_vector; //used for managing free slots
+
+	void (*ctor)(void*); //object constructor
+
 	struct slab_s* next; //used by the cache manager
 }slab_s;
 
 typedef struct kmem_cache_s{
+	//slab list heads
 	slab_s* empty_slabs;
 	slab_s* partial_slabs;
 	slab_s* full_slabs;
+
 	int count_empty;
 	int count_partial;
 	int count_full;
+
 	int obj_size;
 	int num_of_allocated_objs;
 	int objs_per_slab;
 	int num_of_blocks;
+
+	//how many errors has this cache managed
 	int num_of_errors;
+
+	//used for L1 cache alignment
+	int wasted;
+	int offset;
+
+	//cache name
 	char name[30];
+
 	void (*ctor)(void*);
 	void (*dtor)(void*);
+
+	//used for making a linked list of caches
 	struct kmem_cache_s* next;
 }kmem_cache_s;
 
-slab_s* allocate_a_slab(int obj_size, int* ret_param); //allocate a new slab
+slab_s* allocate_a_slab(int obj_size, int* objs_per_slab_ret, void(*ctor)(void*), int* wasted_ret, int offs); //allocate a new slab
 void deallocate_a_slab(slab_s* sl); //free a slab
 
 void print_slab(slab_s* sl); //print slab info
 
-void* allocate_obj_from_slab(slab_s* sl, void(*ctor)(void*)); //allocate an object and initialize it
+void* allocate_obj_from_slab(slab_s* sl); //allocate an object and initialize it
 int dealloc_obj_from_slab(slab_s* sl, void* obj, void(*dtor)(void*)); //return 0 if dealloc is successful
 
-//helper function
-int is_obj_valid(slab_s* sl, void* obj); //check if the pointer to dealloc is valid
+//helper functions
+int get_obj_index(slab_s* sl, void* obj); //check if the pointer to dealloc is valid
+void* get_obj_address(slab_s* sl, int index);
+
+void print_bit_vect(slab_s* sl);
+
+void set_bit(slab_s* sl, int index, int bit);
+int get_bit(slab_s* sl, int index);
 
 #endif
